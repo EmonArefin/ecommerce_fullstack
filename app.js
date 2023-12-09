1abb114aa230532194b571860c63daec9f5d9ee7
EmonArefin <shovon1218@gmail.com>
2023-12-09 12:40:29 +0600

    connection made between backend and frontend

--- app.js
+++ app.js
@@ -37,9 +37,14 @@ mongoose
         console.log(error);
     })
 
+app.use(express.static('client/dist'))
+
+app.get("*", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+})
+
 app.use("/api", router)
 
-app.use(express.static('client/dist'))
 app.get("*", (req, res)=>{
     res.status(404).json({
         status: "Fail",
